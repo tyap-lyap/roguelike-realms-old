a40b00060ae0d3c5f8f5a2b72424322666764b05
Doctor Night <72402692+DoctorNight799@users.noreply.github.com>
2020-10-26 12:21:14 +0300

    advencements
    Создал Древо ачивок

--- src/main/java/net/sfedunet/AnyItemsMod.java
+++ src/main/java/net/sfedunet/AnyItemsMod.java
@@ -9,7 +9,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import net.sfedunet.instruments.*;
 
 public class AnyItemsMod implements ModInitializer {
 	public static final Item ECHPOCHMAK = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().saturationModifier(10f).hunger(14).statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE,20*5),0.5f).statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 20*5),0.5f).build()));
