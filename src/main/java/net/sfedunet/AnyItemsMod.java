62fb343b0d27b4f136fcf565f43c9a7f9b2f88c0
Doctor Night <72402692+DoctorNight799@users.noreply.github.com>
2020-10-26 15:16:43 +0300

    LOOT TABLE ADDED
    С зомби лук падает и крафт эчпочмака сделан

--- src/main/java/net/sfedunet/AnyItemsMod.java
+++ src/main/java/net/sfedunet/AnyItemsMod.java
@@ -12,8 +12,11 @@ import net.minecraft.util.registry.Registry;
 
 public class AnyItemsMod implements ModInitializer {
 	public static final Item ECHPOCHMAK = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().saturationModifier(10f).hunger(14).statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE,20*5),0.5f).statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 20*5),0.5f).build()));
+	public static final Item ONION = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().saturationModifier(2f).hunger(1).statusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 20*10),1f).build()));
 	@Override
-	public void onInitialize() {
+	public void onInitialize()
+	{
 		Registry.register(Registry.ITEM, new Identifier("modid", "echpochmak"), ECHPOCHMAK);
+		Registry.register(Registry.ITEM, new Identifier("modid", "onion"), ONION);
 	}
 }
