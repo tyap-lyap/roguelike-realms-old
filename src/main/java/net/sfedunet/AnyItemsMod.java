3846060ec8db3c9d7f0276ca5fc4a86791327868
Doctor Night <72402692+DoctorNight799@users.noreply.github.com>
2020-10-26 10:47:59 +0300

    echpochmak added
    Добавлен эчпочмак и небольшие изменения в проекте

--- src/main/java/net/sfedunet/AnyItemsMod.java
+++ src/main/java/net/sfedunet/AnyItemsMod.java
@@ -1,10 +1,20 @@
 package net.sfedunet;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.sfedunet.instruments.*;
 
 public class AnyItemsMod implements ModInitializer {
+	public static final Item ECHPOCHMAK = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().saturationModifier(10f).hunger(14).statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE,20*5),0.5f).statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 20*5),0.5f).build()));
 	@Override
 	public void onInitialize() {
-
+		Registry.register(Registry.ITEM, new Identifier("modid", "echpochmak"), ECHPOCHMAK);
 	}
 }
